a64b8c20aaf2413d66b99dee99b8f0a0ae7ca8ab
gyomdyung <seastory624@gmail.com>
2022-09-11 14:10:22 +0900

    Feat: payment 컬럼 Swagger 추가

--- src/api/payment/entities/payment.entity.ts
+++ src/api/payment/entities/payment.entity.ts
@@ -1,17 +1,33 @@
 import { CommonEntity } from './../../../common/entities/common-entity';
 import { Column, Entity } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Payment extends CommonEntity {
+  @ApiProperty({
+    example: '배송 상태',
+    required: true,
+  })
   @Column()
   pay_state: string;
 
+  @ApiProperty({
+    example: '주문 수량',
+    required: true,
+  })
   @Column()
   quantity: number;
 
+  @ApiProperty({
+    example: '결제 금액',
+    required: true,
+  })
   @Column()
   price: number;
 
+  @ApiProperty({
+    example: '운송장 번호',
+  })
   @Column()
-  delivery_num: string;
+  delivery_num: string | null;
 }
--- src/api/payment/payment.service.ts
+++ src/api/payment/payment.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
 
 @Injectable()
 export class PaymentService {}
