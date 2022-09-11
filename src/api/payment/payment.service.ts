1248d60873525e9b984a6b1919d4048ebedf3f71
gyomdyung <seastory624@gmail.com>
2022-09-11 17:15:58 +0900

    Refactor: 주문내역 리스트 가져오기 API 쿼리 변경

--- src/api/payment/payment.service.ts
+++ src/api/payment/payment.service.ts
@@ -30,10 +30,46 @@ export class PaymentService {
   }
 
   async getPaymentList() {
-    const list = await this.paymentRepository.find();
+    const list = await this.paymentRepository
+      .createQueryBuilder('payment')
+      .leftJoinAndSelect('payment.buyr', 'buyr')
+      .select([
+        'payment.createAt as Date',
+        'payment.pay_state as pay_state',
+        'payment.quantity as quantity',
+        'payment.price as price',
+        'buyr.city',
+        'buyr.country',
+        'buyr.zpix',
+        'buyr.vccode',
+        'payment.delivery_num as delivery_num',
+      ])
+      .getRawMany();
 
     return list;
   }
+  /* 
+      {
+            "payment_id": 1,
+            "payment_create_at": "2022-09-11T07:41:04.625Z",
+            "payment_update_at": "2022-09-11T07:41:04.625Z",
+            "payment_delete_at": null,
+            "payment_pay_state": "결제완료",
+            "payment_quantity": 2,
+            "payment_price": 10000,
+            "payment_delivery_num": "450784629761",
+            "payment_buyr_id": 1,
+            "buyr_id": 1,
+            "buyr_create_at": "2022-09-11T07:33:21.491Z",
+            "buyr_update_at": "2022-09-11T07:33:21.491Z",
+            "buyr_delete_at": null,
+            "buyr_name": "장성우",
+            "buyr_city": "incheon",
+            "buyr_zpix": "20902",
+            "buyr_vccode": 82,
+            "buyr_country": "KR"
+        },
+  */
 
   async getPaymentFilter(status: string) {
     const queryResult = await this.paymentRepository
